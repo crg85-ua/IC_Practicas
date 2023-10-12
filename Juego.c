b15f2a191f04bb274a9abb7a56e644c8733fcb37
cris2795 <csm150@alu.ua.es>
2023-10-12 02:07:53 +0200

    fallo en el 3 scanf :(

--- Juego.c
+++ Juego.c
@@ -4,8 +4,9 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define NUM_PLAYERS_MAX 10
-#define BOARD_SIZE 20
+#define NUM_PLAYERS_MAX 20
+#define BOARD_SIZE_FILA 50
+#define BOARD_SIZE_COLUMNA 10
 
 int getRandomStep() {
     return rand() % 6;
@@ -13,7 +14,7 @@ int getRandomStep() {
 
 void printBoard(int positions[], int numPlayers) {
     printf("Tablero:\n");
-    for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int i = 0; i < BOARD_SIZE_FILA; i++) {
         int playerIndex = -1;
         for (int j = 0; j < numPlayers; j++) {
             if (positions[j] == i) {
@@ -30,11 +31,11 @@ void printBoard(int positions[], int numPlayers) {
     printf("|\n");
 }
 
-int main() {
-    int numPlayers, choise, steps, result, c;
+int pedirNumero(){
+    int numPlayers,result,c;
     bool valid = false;
     while (valid == false){
-        // Pedimos al usuario el número de jugadores y si desea usar números random o fijos
+        // Pedir al usuario el número de jugadores y si desea usar números random o fijos
         printf("Ingrese el número de jugadores (hasta %d): \n", NUM_PLAYERS_MAX);
         result = scanf("%d", &numPlayers);
 
@@ -42,27 +43,27 @@ int main() {
             printf("Debe de introducir un número, inténtelo de nuevo\n");
             while ((c = getchar()) != '\n' && c != EOF) {}
         } else if (numPlayers < 2 || numPlayers > NUM_PLAYERS_MAX) {
-            printf("Número de jugadores no válido. Debe ser entre 2 y %d.\n, inténtelo de nuevo.\n", NUM_PLAYERS_MAX);
-            return 1;
+            printf("Número de jugadores no válido. Debe ser entre 2 y %d, inténtelo de nuevo.\n", NUM_PLAYERS_MAX);
         } else{
             valid = true;
         }
     }
-    
-    valid = false;
+    return numPlayers;
+}
 
+int pedirAvanzar(){
+    int choise, steps,c,result;
+    bool valid = false;
     while (valid == false){
         printf("¿Desea usar números aleatorios para avanzar? (1 para Sí, 0 para No): \n");
         result = scanf("%d", &choise);
-        
         if (result != 1){
             printf("Debe de introducir un número, inténtelo de nuevo\n");
-            while ((c = getchar()) != '\n' && c != EOF) {}
-        } else if(choise == 0){
+           while ((c = getchar()) != '\n' && c != EOF) {}
+       } else if(choise == 0){
             while (valid == false){
                 printf("Elija un número del 1 al 5\n");
                 result = scanf("%d", &steps);
-
                 if (result != 1){
                 printf("Debe de introducir un número, inténtelo de nuevo\n");
                 while ((c = getchar()) != '\n' && c != EOF) {}
@@ -73,62 +74,113 @@ int main() {
                 }
             }
         } else if (choise == 1){
-            valid == true;
+            valid = true;
             steps = 0;
         } else {
-            printf("Opción no válida. Debe ser 0 o 1.\n, inténtelo de nuevo");
-            return 1;
+            printf("Opción no válida. Debe ser 0 o 1, inténtelo de nuevo\n");
         }
     }
+    return steps;
+}
+
+int pedirExponente(){
+    int choise, exponent,c,result;
+    bool valid = false;
+    while (valid == false){
+        printf("¿Desea usar números aleatorios para el exponente? (1 para Sí, 0 para No): \n");
+        result = scanf("%d", &choise);
+        printf("aaa");
+        if (result != 1){
+            printf("Debe de introducir un número, inténtelo de nuevo\n");
+            while ((c = getchar()) != '\n' && c != EOF) {}
+        } else if(choise == 0){
+            while (valid == false){
+                printf("Elija un número para el exponent\n");
+                result = scanf("%d", &exponent);
+
+                if (result != 1){
+                    printf("Debe de introducir un número, inténtelo de nuevo\n");
+                    while ((c = getchar()) != '\n' && c != EOF) {}
+                } else if (exponent < 0){
+                    printf("Nuḿero erróneo, tiene que ser mayor que 0\n");
+                } else{
+                    valid = true;
+                }
+            }
+        } else if (choise == 1){
+            valid = true;
+            exponent = 0;
+        } else {
+            printf("Opción no válida. Debe ser 0 o 1, inténtelo de nuevo\n");
+        }
+    }
+    return exponent;
+}
+
+int main() {
+    int numPlayers, steps ,exponent;
+    bool valid = false;
+    bool validAux = false;
+    numPlayers = pedirNumero();
+    steps = pedirAvanzar();
+    exponent = pedirExponente();
     
+
     srand(time(NULL)); // Inicializamos la semilla para números aleatorios
     
-    int board[BOARD_SIZE] = {0};
-    int positions[NUM_PLAYERS_MAX] = {0};
+    int positions[NUM_PLAYERS_MAX] = {(0,0)};
+    int board[BOARD_SIZE_FILA][BOARD_SIZE_COLUMNA] = {0};
+    for(int i = 0; i < numPlayers; i+2){
+        board[0][i] = i;
+        board[BOARD_SIZE_FILA - 1][i] = i+1; 
+    }
     
     while (1) {
         for (int i = 0; i < numPlayers; i++) {
-            // Mover jugadorFALSE
-            if (choise) {
+            // Movemos jugador
+            if (steps == 0) {
                 positions[i] += getRandomStep();
             } else {
                 positions[i] += steps;
             }
             
-            // Verificamos si el jugador ha llegado al final del tablero
-            if (positions[i] >= BOARD_SIZE) {
+            // Verificamos si el jugador llegó al final del tablero
+            if (positions[i] >= BOARD_SIZE_FILA) {
                 printf("Jugador %d ha llegado al final del tablero.\n", i + 1);
-                positions[i] = -1; // Marcar al jugador como terminado
+                positions[i] = -1; // Marcamos al jugador como que ha terminado
             }
             
             // Verificamos colisiones
             for (int j = 0; j < numPlayers; j++) {
                 if (i != j && positions[i] == positions[j]) {
-                    int exponent = choise ? (rand() % 6) : steps;
-                    int time1 = (int)pow(3, exponent);
-                    int time2 = (int)pow(3, exponent);
+                    int exponentAux;
+                    if(exponent == 0){
+                        steps = rand() % 6;
+                    }
+                    int time1 = (int)pow(3, exponentAux);
+                    int time2 = (int)pow(3, exponentAux);
                     
                     printf("¡Colisión entre Jugador %d y Jugador %d!\n", i + 1, j + 1);
-                    printf("Jugador %d tarda %d segundos en resolver 3^%d.\n", i + 1, time1, exponent);
-                    printf("Jugador %d tarda %d segundos en resolver 3^%d.\n", j + 1, time2, exponent);
+                    printf("Jugador %d tarda %d segundos en resolver 3^%d.\n", i + 1, time1, exponentAux);
+                    printf("Jugador %d tarda %d segundos en resolver 3^%d.\n", j + 1, time2, exponentAux);
                     
                     if (time1 < time2) {
                         printf("Jugador %d ha sido eliminado.\n", j + 1);
-                        positions[j] = -1; // Marcar al jugador como eliminado
+                        positions[j] = -1; // Marcamos al jugador como eliminado
                     } else if (time2 < time1) {
                         printf("Jugador %d ha sido eliminado.\n", i + 1);
-                        positions[i] = -1; // Marcar al jugador como eliminado
+                        positions[i] = -1; // Marcamos al jugador como eliminado
                     } else {
                         printf("Empate. Ambos jugadores continúan.\n");
                     }
                 }
             }
             
-            // Imprimimos el tablero
+            // Imprimir el tablero
             printBoard(positions, numPlayers);
         }
         
-        // Verificamos si todos los jugadores han llegado al final del tablero
+        // Verificar si todos los jugadores han llegado al final del tablero
         int finishedPlayers = 0;
         for (int i = 0; i < numPlayers; i++) {
             if (positions[i] == -1) {
