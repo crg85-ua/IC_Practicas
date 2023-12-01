0628c1cfbe0be7a3e2668cfd44d76b53ef413b8d
apg204-ua <apg204@alu.ua.es>
2023-12-01 17:03:39 +0100

    Added Docker build & script

---
+++ p3/unraw/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p build
+docker build -t unraw .
+docker run -it -v ".:/root/app" unraw sh -c "\
+    cd /root/app && \
+    cd build && \
+    cmake .. && \
+    make -j$(nproc) unraw && \
+    cd .. && \
+    time ./build/unraw _DSC2078.NEF _DSC2078.jpeg"
